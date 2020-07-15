6c6c45fe726c4ca3a88a601bc57bea671c9e4c55
Christin Koshy <christinkoshy1998@gmail.com>
2020-07-15 15:18:53 +0530

    ThirdPartyLibrariesActivity done Instable build

--- app/src/main/java/com/project/christinkcdev/share/shareindia/activity/ThirdPartyLibrariesActivity.java
+++ app/src/main/java/com/project/christinkcdev/share/shareindia/activity/ThirdPartyLibrariesActivity.java
@@ -1,16 +1,32 @@
 package com.project.christinkcdev.share.shareindia.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.project.christinkcdev.share.shareindia.R;
+import com.project.christinkcdev.share.shareindia.app.Activity;
 
-public class ThirdPartyLibrariesActivity extends AppCompatActivity {
+public class ThirdPartyLibrariesActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_party_libraries);
+
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home)
+            onBackPressed();
+        else
+            return super.onOptionsItemSelected(item);
+
+        return true;
     }
 }
